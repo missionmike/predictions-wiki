405fdbb49ab78fd7ea61ebaa6e1fa1fcbf440405
Michael Dinerstein <md@missionmike.dev>
2025-04-25 09:08:44 -0700

    fix: pages config.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed from '/predictions/' to '/'
+  base: '/predictions/', // GitHub Pages base URL
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
