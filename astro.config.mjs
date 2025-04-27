300c42dee02da1d9f8f6248388551cf5dccfbc40
Michael Dinerstein <md@missionmike.dev>
2025-04-27 16:26:16 -0700

    feat: fix base url.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://missionmike.github.io",
+  site: 'https://predictions.wiki',
   integrations: [mdx(), sitemap()],
   vite: {
-    envPrefix: ["PUBLIC_"],
-    logLevel: "info",
+    envPrefix: ['PUBLIC_'],
+    logLevel: 'info',
   },
-  output: "static",
+  output: 'static',
   build: {
-    format: "directory",
+    format: 'directory',
   },
 });
