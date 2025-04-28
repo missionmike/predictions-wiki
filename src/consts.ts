cadcdf420015910863bb9e4962f228c1d3f58506
Michael Dinerstein <md@missionmike.dev>
2025-04-27 22:46:59 -0700

    feat: update default description.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Predictions Wiki';
-export const SITE_DESCRIPTION = 'A collection of predictions from the past';
+export const SITE_DESCRIPTION =
+  'Behold a collection of notable predictions made by people over time. ' +
+  "How many of them came true? We're keeping track.";
